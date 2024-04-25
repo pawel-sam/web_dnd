32bbfbbfaa4286831f2eefc2184c8d957745e27b
Krakazyabra21 <krakazyabra2102@mail.ru>
2024-04-26 01:39:27 +0300

    numbered gamefield

--- web_dnd/static/css/script.js
+++ web_dnd/static/css/script.js
@@ -1,24 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const grid = document.querySelector('.grid');
+    const grid = document.querySelector('.grid');
 
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
-    return color;
-  }
+/*    function move(direction){
 
-  for (let i = 0; i < 275; i++) {
+    }*/
+
+    grid.addEventListener('click', function(event) {
+        const clickedSquare = event.target;
+        clickedSquare.style.backgroundColor = getRandomColor();
+    });
+
+    const rows = 11;
+    const cols = 25;
+    let game_field = [];
+    for (let i = 5; i > 5-rows; i--) {
+    game_field[i] = [];
+
+    for (let j = -12; j < cols-12; j++) {
     const square = document.createElement("div");
     square.classList.add("square");
-//    square.style.backgroundColor = getRandomColor(); // Применяем цвет из массива colors
+    square.textContent = `y= ${i} x= ${j}`;
     grid.appendChild(square);
-  }
-
-  grid.addEventListener('click', function(event) {
-    const clickedSquare = event.target;
-    clickedSquare.style.backgroundColor = getRandomColor();
-  });
+    game_field[i][j] = square;
+    console.log(game_field.length);
+        }
+    }
+    game_field[0][0].style.backgroundColor = "red"
 });
